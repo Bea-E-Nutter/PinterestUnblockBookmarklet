4f4b6b327d93e389fb8dc04417f50128fd3cd3bd
Bea Nutter <blaykenutter@gmail.com>
2022-11-21 12:21:29 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ links = document.querySelectorAll("[href]")
 for(link_iterator = 0; link_iterator < links.length; link_iterator = link_iterator + 1) {
     parsed = new URL(links[link_iterator].href)
     if(!(parsed.hostname == "www.pinterest.com")) {continue;} else {
-        new_link = "https://" + parsed.hostname.split(".com")[0] +".in" + parsed.pathname
+        new_link = "https://" + parsed.hostname.split(".com")[0] +".co.uk" + parsed.pathname
         links[link_iterator].attributes.new_url = new_link
         links[link_iterator].href = "javascript:redirect"
         links[link_iterator].onclick = (d) => {
